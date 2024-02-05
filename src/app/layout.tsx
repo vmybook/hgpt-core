7898f90ec82f9f7b3a1a7f9dd887e1a9963d6e56
Artem Verkhoturov <verhoturov@mybook.ru>
2024-02-06 01:09:44 +0300

    refactore: move ui to shared

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
+import { cn } from "@/shared/lib/utils";
 
 const fontSans = FontSans({
   subsets: ["latin"],
