9b119c12fcfaff2dfffe60e9d590f84f203d0f47
Artem Verkhoturov <verhoturov@mybook.ru>
2024-02-06 01:12:55 +0300

    refactor: prisma client as singlton

--- src/shared/lib/db.ts
+++ src/shared/lib/db.ts
@@ -1,3 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-export const dbClient = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+declare global {
+  var dbClient: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const dbClient = globalThis.dbClient ?? prismaClientSingleton()
+
+export default dbClient
+
+if (process.env.NODE_ENV !== 'production') globalThis.dbClient = dbClient
