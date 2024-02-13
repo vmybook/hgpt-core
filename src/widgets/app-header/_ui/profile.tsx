499d4f777a603b583eac0d157f4a6024212ee86d
Artem Verkhoturov <verhoturov@mybook.ru>
2024-02-13 19:48:01 +0300

    feat: add base auth

--- src/widgets/app-header/_ui/profile.tsx
+++ src/widgets/app-header/_ui/profile.tsx
@@ -12,9 +12,28 @@ import {
 import { LogOut, User } from 'lucide-react';
 import { Button } from '@/shared/components/ui/button';
 import Link from 'next/link';
-import { Avatar, AvatarFallback } from '@/shared/components/ui/avatar';
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from '@/shared/components/ui/avatar';
+import { useAppSession } from '@/entities/session/use-app-session';
+import { useSignOut } from '@/features/auth/use-sign-out';
+import { SignInButton } from '@/features/auth/sign-in-button';
+import { Skeleton } from '@/shared/components/ui/skeleton';
 
 export function Profile() {
+    const session = useAppSession();
+    const { signOut, isPending: isLoadingSignOut } = useSignOut();
+
+    if (session.status === 'loading') {
+        return <Skeleton className="w-8 h-8 rounded-full" />;
+    }
+
+    if (session.status === 'unauthenticated') {
+        return <SignInButton />;
+    }
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
@@ -23,6 +42,7 @@ export function Profile() {
                     className="p-px rounded-full self-center h-8 w-8"
                 >
                     <Avatar className="w-8 h-8">
+                        <AvatarImage src={session.data?.user.image} />
                         <AvatarFallback>AN</AvatarFallback>
                     </Avatar>
                 </Button>
@@ -31,7 +51,7 @@ export function Profile() {
                 <DropdownMenuLabel>
                     <p>Мой аккаунт</p>
                     <p className="text-xs text-muted-foreground overflow-hidden text-ellipsis">
-                        Nikolaev
+                        {session.data?.user.name}
                     </p>
                 </DropdownMenuLabel>
                 <DropdownMenuGroup></DropdownMenuGroup>
@@ -43,7 +63,10 @@ export function Profile() {
                             <span>Профиль</span>
                         </Link>
                     </DropdownMenuItem>
-                    <DropdownMenuItem>
+                    <DropdownMenuItem
+                        disabled={isLoadingSignOut}
+                        onClick={() => signOut()}
+                    >
                         <LogOut className="mr-2 h-4 w-4" />
                         <span>Выход</span>
                     </DropdownMenuItem>
