34c919947cef532eddd89310567653bd80e9d545
Artem Verkhoturov <verhoturov@mybook.ru>
2024-02-13 06:54:17 +0300

    feat: redisign header

--- src/widgets/app-header/_ui/layout.tsx
+++ src/widgets/app-header/_ui/layout.tsx
@@ -19,7 +19,7 @@ export function Layout({
     actions?: React.ReactNode;
 }) {
     return (
-        <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 shadow-md">
             <div className="container flex h-14 items-center">
                 <div className="md:hidden mr-2">
                     <Sheet>
